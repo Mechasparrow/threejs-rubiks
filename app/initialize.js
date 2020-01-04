b9ee0c2f8a0e0e3ecabf69aed48243d212130302
Michael Navazhylau <mikipux7@gmail.com>
2020-01-03 19:44:48 -0600

    reoriented some stuff

--- app/initialize.js
+++ app/initialize.js
@@ -3,6 +3,7 @@ Entry point for the program
 **/
 
 var Main = require('main.js');
+var Timer = require('easytimer.js').Timer;
 
 document.addEventListener('DOMContentLoaded', () => {
   Main.main();
